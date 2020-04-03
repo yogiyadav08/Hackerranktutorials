4780f4628fce71cee409a773d2a0df92d81a895f
Yogendra Kumar <60997584+yogiyadav08@users.noreply.github.com>
2020-04-04 00:27:18 +0530

    Update binaryCal.js

--- Day 9/Binary Calculator/binaryCal.js
+++ Day 9/Binary Calculator/binaryCal.js
@@ -1,3 +1,5 @@
+Method 1:
+
 let elem = document.getElementById("res");
   
   function checkOperator(){
@@ -80,3 +82,36 @@ let elem = document.getElementById("res");
       alert("Line must ends with number.")
     }
   }
+
+
+
+
+Method 2:
+function onButton(e) {
+    var btn = e.target || e.srcElement;
+    var action = document.getElementById(btn.id).innerHTML;
+    var res = document.getElementById('res');
+    
+    switch(action) {
+        case 'C':
+            res.innerHTML = '';
+            break;
+        case '=':
+            var expr = res.innerHTML;
+            var nums = /(\d+)/g;
+            // Replace all base 2 nums with base 10 equivs
+            expr = expr.replace(nums, function(match) {
+                return parseInt(match, 2);
+            })
+            // eval in base 10 and convert to base 2
+            res.innerHTML = eval(expr).toString(2);
+            break;
+        default:
+            res.innerHTML += action;
+            break;
+    }
+}
+var buttons = document.getElementsByTagName('button');
+for (let button of buttons) {
+    button.onclick = onButton;
+}
